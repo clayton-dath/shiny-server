774ac32095b20eb9be6ce6b4a56a30664e6190f9
clayton-dath <clayton.dath@gmail.com>
2018-07-06 09:28:13 +1200

    Update and rename ui.R to app.R

--- groceries/app.R
+++ groceries/app.R
@@ -1,5 +1,7 @@
 library(shiny)
 library(shinydashboard)
+library(dplyr)
+
 
 meal.list <- read.csv('data/meal.csv')
 
@@ -34,3 +36,24 @@ dashboardPage(
           , body
           )
 
+function(input, output) {
+
+  meals = reactive(meal.list[,c(2,3,4,5)])
+  
+  grocery_list = reactive(meals()[meals()$Main.Ingredient == 'y',] %>%
+                            group_by(Ingredient) %>%
+                            summarise(Measurement = first(Measurement), Quantity = sum(Quantity)) %>%
+                            as.data.frame()
+                          )
+  condiments = reactive(meals()[meals()$Main.Ingredient == 'n',] %>%
+                          group_by(Ingredient) %>%
+                          summarise(Measurement = first(Measurement), Quantity = sum(Quantity)) %>%
+                          as.data.frame()
+                        )
+  
+  ## Groceries
+  output$grocery_list = renderDataTable(grocery_list())
+  output$condiments = renderDataTable(meals())
+  
+}
+      
